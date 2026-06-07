73fb41e26e47f3f443b279270c7a4dde52da8f58
GitHub Action <notifications@github.com>
2026-06-07 15:11:56 +0000

    Automated Terraform Formatting

--- dynamic_main.tf
+++ dynamic_main.tf
@@ -351,8 +351,8 @@ resource "random_password" "os_linuxadmin_password" {
 }
 
 resource "random_password" "os_appuser_password" {
-  length  = 32
-  special = true
+  length           = 32
+  special          = true
   override_special = "!#%&*()-_=+[]{}<>"
 }
 
@@ -415,8 +415,8 @@ POLICY
 
 # OS Secrets Engine Mount
 resource "vault_mount" "os_mount" {
-  namespace = vault_namespace.demo.path_fq
-  path      = "os"
+  namespace   = vault_namespace.demo.path_fq
+  path        = "os"
   type        = "vault-plugin-secrets-os"
   description = "OS secret backend"
 }
@@ -447,7 +447,7 @@ resource "vault_os_secret_backend_host" "web_dynamic" {
   namespace  = vault_namespace.demo.path_fq
   mount      = vault_os_secret_backend.os_backend.mount
   name       = "web-dynamic"
-  
+
   # Using the public IP of the created web server so external Vault can reach it
   address         = module.web_dynamic.public_ip
   port            = 22
